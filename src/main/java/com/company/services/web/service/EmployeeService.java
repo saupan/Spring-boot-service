16350d7df08f13b923aada0d5ad995763a306e2c
Sauveer Pandey <sauveer.pandey@sabre.com>
2018-05-21 14:00:41 +0200

    Added unit test for EmployeeService.java

--- src/main/java/com/company/services/web/service/EmployeeService.java
+++ src/main/java/com/company/services/web/service/EmployeeService.java
@@ -2,30 +2,25 @@ package com.company.services.web.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.company.services.web.exception.ResourceNotFoundException;
 import com.company.services.web.model.Employee;
-import com.company.services.web.model.Hobby;
 import com.company.services.web.repository.EmployeeRepository;
-import com.company.services.web.repository.HobbyRepository;
 
 @Transactional
 @Service
 public class EmployeeService {
+	
+	private final EmployeeRepository employeeRepository;
 
 	@Autowired
-	EmployeeRepository employeeRepository;
-
+	public EmployeeService(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
 
 	public List<Employee> getAllEmployees() {
 		List<Employee> employees = new ArrayList<Employee>();
--- src/test/java/com/company/services/web/service/EmployeeServiceTest.java
+++ src/test/java/com/company/services/web/service/EmployeeServiceTest.java
@@ -1,59 +1,59 @@
 package com.company.services.web.service;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.ArgumentMatchers.*;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.company.services.web.model.Employee;
 import com.company.services.web.repository.EmployeeRepository;
 
 
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class EmployeeServiceTest {
 
-	@TestConfiguration
-	static class EmployeeServiceImplTestContextConfiguration {
-
-		@Bean
-		public EmployeeService employeeService() {
-			return new EmployeeService();
-		}
-	}
-
-	@Autowired
-	private EmployeeService employeeService;
-
-	@MockBean
+	@Mock	
 	private EmployeeRepository employeeRepository;
 
 	
 	@Test
 	public void whenValidId_thenEmployeeShouldBeFound() {
- 
-		doReturn(Optional<Employee>.of(new Employee())).when(employeeRepository).findById(anyLong());        
+		EmployeeService employeeService = new EmployeeService(employeeRepository);
+		Mockito.doReturn(Optional.of(new Employee())).when(employeeRepository).findById(anyLong());        
         assertNotNull(employeeService.getEmployeeById(1L));
 	 }
 	
-	//GET /employees (all employees are found)
-	//POST /employees (EmployeeIsAdded)
-	//GET /employees/id (WhenIdNotFound_then404IsThrown)
-	//whenIdisValid_thenEmployeeShouldBeFound
-	//PUT /employees/id (WhenIdNotFound_then404IsThrown)
-	//WhenIdValid_thenEmployeeShouldBeUpdated
-	//DELETE /employees/id (WhenIdNotFound_then404IsThrown)
-	//WhenIdValid_thenEmployeeShouldBeDeleted
+	@Test
+	public void findAllEmployeesTest() {
+		EmployeeService employeeService = new EmployeeService(employeeRepository);
+		Mockito.doReturn(new ArrayList<Employee>()).when(employeeRepository).findAll(); 
+		assertNotNull(employeeService.getAllEmployees());
+	}
 	
+	@Test
+	public void updateEmployeeTest() {
+		EmployeeService employeeService = new EmployeeService(employeeRepository);
+		Mockito.doReturn(Optional.of(new Employee())).when(employeeRepository).findById(anyLong());
+		Mockito.doReturn(new Employee()).when(employeeRepository).save(any());
+		assertNotNull(employeeService.updateEmployee(2L, new Employee()));
+	}
+	
+	
+	@Test
+	public void deleteEmployeeTest() {
+		EmployeeService employeeService = new EmployeeService(employeeRepository);
+		Mockito.doReturn(Optional.of(new Employee())).when(employeeRepository).findById(anyLong());
+		Mockito.doNothing().when(employeeRepository).delete(any());
+		employeeService.deleteEmployee(anyLong());
+	}
 	
 }
 
